e2ac00897531d5d45ab03efa265c11a453203ffa
Jay Lingelbach <jay.lingelbach@gmail.com>
2018-06-10 16:00:26 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello, World!</Text>
+      </View>
+    );
+  }
+}
+
+class Todo extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Todo</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
